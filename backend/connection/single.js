2f0eae9379b5df1e72cf7486f63ab0dcd485b258
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2018-09-14 19:58:23 -0700

    fixing Error: Cannot enqueue Query after invoking quit.

--- backend/connection/single.js
+++ backend/connection/single.js
@@ -13,19 +13,19 @@ function query(query, params, callback) {
     let connection = getConnection();
     connection.connect(function (err) {
         if (err) {
-            connection.end();
+            
             console.log(`DB connection Error: ${err.message}`);
         } else {
             connection.query(query, params, function (error, results, fields) {
-                connection.end();
+               
                 if (error) {
                     console.log(`DB query Error: ${err.message}`);
                 } else {
-                    callback(results, fields);
+                    callback(error, results, fields);
                 }
             })
         }
-    })
+    });
 }
 
 module.exports.singleQuery = query;
