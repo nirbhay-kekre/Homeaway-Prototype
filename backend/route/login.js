19e5763d8caa703270672045e4ad4e04e3a645a6
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2018-09-16 16:28:05 -0700

    adding role and username to session login.js route

--- backend/route/login.js
+++ backend/route/login.js
@@ -1,17 +1,18 @@
 let express = require("express");
 let query = require("../connection/pool").poolQuery;
 let bycrypt = require("bcrypt");
+
 let router = express.Router;
 
 router.post("/login", (req, res) => {
-    let userNameActual = req.body.userName;
+    let usernameActual = req.body.username;
     let passwordActual = req.body.password;
-    macthCredentialsInDB(userNameActual, passwordActual, res);
+    macthCredentialsInDB(usernameActual, passwordActual, res);
 });
 
-function macthCredentialsInDB(userNameActual, passwordActual, resp) {
+function macthCredentialsInDB(usernameActual, passwordActual, resp) {
 
-    query(`SELECT username, password from credentials where username= ?`,[userNameActual], function (error, records, fields) {
+    query(`SELECT username, password from credentials where username= ?`,[usernameActual], function (error, records, fields) {
         if (error) {
             console.log(`Error: ${error.message}`);
             resp.writeHead(500, {
@@ -25,6 +26,9 @@ function macthCredentialsInDB(userNameActual, passwordActual, resp) {
             let record = records[0];
             bycrypt.compare(passwordActual, record.password, function (err, result) {
                 if (result) {
+                    // res.cookie('cookie',username,{maxAge: 900000, httpOnly: false, path : '/'});
+                    req.session.username = record.username;
+                    req.session.role= record.role;
                     resp.writeHead(200, {
                         'Content-Type': 'application/json'
                     });
