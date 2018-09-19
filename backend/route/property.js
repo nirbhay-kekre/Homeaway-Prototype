b611f534f6979030c36a1ff5e38c5a9e7674511a
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2018-09-18 21:33:52 -0700

    adding backend code for searching, filtering and detail view of property

--- backend/route/property.js
+++ backend/route/property.js
@@ -1,9 +1,10 @@
 let express = require("express");
 let query = require("../connection/pool").poolQuery;
+let DateDiff = require("date-diff");
 
 let router = express.Router;
 
-router.use("/createProperty", function(req,resp, next){
+router.use("/createProperty", function (req, resp, next) {
     req.checkBody("bedroom", "bedroom NAN").matches(/^[0-9]$/);
     req.checkBody("bathroom", "bathroom NAN").matches(/^[0-9]$/);
     req.checkBody("zip", "zip NAN").matches(/^[0-9]{5}$/);
@@ -13,7 +14,7 @@ router.use("/createProperty", function(req,resp, next){
     req.checkBody("country", "country is empty").notEmpty();
 
     let errors = req.validationErrors();
-    if(errors){
+    if (errors) {
         let msg = errors.map(error => error.msg).reduce((accumulator, currentVal) => accumulator + "\n" + currentVal);
         resp.writeHead(400, {
             'Content-Type': 'application/json'
@@ -22,18 +23,19 @@ router.use("/createProperty", function(req,resp, next){
             success: false,
             message: msg
         }));
-    }else{
+    } else {
         next();
     }
 });
 
-router.post("/createProperty", function(req, resp){
+router.post("/createProperty", function (req, resp) {
     changeRoleToOwner();
-    let {bedroom, bathroom, street, city, state, zip, country, unit}=req.body;
-    let data ={ username: req.session.username,
-        street:street, city: city, unit:unit, 
-        state: state, zip:zip, country: country,
-        bathroom:bathroom, bedroom:bedroom
+    let { bedroom, bathroom, street, city, state, zip, country, unit } = req.body;
+    let data = {
+        username: req.session.username,
+        street: street, city: city, unit: unit,
+        state: state, zip: zip, country: country,
+        bathroom: bathroom, bedroom: bedroom
     };
     insertData(resp, data);
 });
@@ -41,10 +43,10 @@ router.post("/createProperty", function(req, resp){
 /* owner role validator is added after "/createProperty" because,
  if any user creates a property we assign him an owner role */
 
-router.use("/", function (req,resp, next){
-    if(req.session.user && req.session.role == "owner"){
+router.use("/", function (req, resp, next) {
+    if (req.session.username && req.session.role == "owner") {
         next();
-    }else{
+    } else {
         resp.writeHead(401, {
             'Content-Type': 'application/json'
         });
@@ -56,22 +58,229 @@ router.use("/", function (req,resp, next){
 });
 
 
-router.post("/updateProperty", function(req, resp){
-    let {bedroom, bathroom, street, city, state, zip, country, unit,
-    propertyId, headline, propertyDescription, propertyType,
-    accomodates, bookingOption, oneNightRate, minNightStay}=req.body;
-    let data ={ 
-        street:street, city: city, unit:unit, 
-        state: state, zip:zip, country: country,
-        bathroom:bathroom, bedroom:bedroom,
+router.post("/updateProperty", function (req, resp) {
+    let { bedroom, bathroom, street, city, state, zip, country, unit,
+        propertyId, headline, propertyDescription, propertyType,
+        accomodates, bookingOption, oneNightRate, minNightStay, isActive } = req.body;
+    let data = {
+        street: street, city: city, unit: unit,
+        state: state, zip: zip, country: country,
+        bathroom: bathroom, bedroom: bedroom,
         headline: headline, propertyDescription: propertyDescription,
-        propertyType: propertyType, accomodates: accomodates ,
-        bookingOption: bookingOption,oneNightRate: oneNightRate, 
-        minNightStay: minNightStay
+        propertyType: propertyType, accomodates: accomodates,
+        bookingOption: bookingOption, oneNightRate: oneNightRate,
+        minNightStay: minNightStay, isActive: isActive
     };
-    updateData(req,resp,data,propertyId);
+    updateData(req, resp, data, propertyId);
 });
 
+router.get("/search/list", function (req, resp) {
+    let sqlQuery = createListPropertiesQuery(req);
+    query(sqlQuery, [], function (error, records, fields) {
+        if (error) {
+            console.log(error);
+            resp.writeHead(500, {
+                'Content-Type': 'application/json'
+            });
+            resp.end(JSON.stringify({
+                success: false,
+                message: "Internal server error",
+            }));
+        } else {
+            if (records && records.length > 0) {
+                let list = [], data = {};
+                for (let i = 0; i < records.length; i++) {
+                    const record = records[i];
+                    if (data[record.propertyId] === undefined) {
+                        data = {};
+                        data[record.propertyId] = {};
+                        data[record.propertyId].amenities = new Set();
+                        data[record.propertyId].photoUrl = new Set();
+                        data[record.propertyId] = {
+                            propertyId: record.propertyId,
+                            headline: record.headline,
+                            propertyType: record.propertyType,
+                            bedroom: record.bedroom,
+                            bathroom: record.bedroom,
+                            accomodates: record.accomodates,
+                            street: record.street,
+                            city: record.city,
+                            state: record.state,
+                            zip: record.zip,
+                            country: record.country,
+                            oneNightRate: record.oneNightRate
+                        };
+                        data["rank"] = i;    // will be used to maintain sorted result from the sql query, as amenity and photo are not sorting criteria
+                        list.push(data);
+                    }
+                    if (record.amenity) {
+                        data[record.propertyId].amenities.add(amenity);
+                    }
+                    if (record.photoUrl) {
+                        data[record.propertyId].photoUrl.add(record.photoUrl);
+                    }
+                }
+                list.sort((property1, property2) => property1.rank - property2.rank);
+                resp.writeHead(200, {
+                    'Content-Type': 'application/json'
+                });
+                resp.end(JSON.stringify({
+                    success: true,
+                    message: `${list.length} results found`,
+                    results: list
+                }));
+            } else {
+                resp.writeHead(204, {
+                    'Content-Type': 'application/json'
+                });
+                resp.end(JSON.stringify({
+                    success: true,
+                    message: "No Content",
+                }));
+            }
+        }
+    })
+});
+
+router.get("/search/detail", function (req, resp) {
+    let sqlQuery = createDetailPropertyQuery(req);
+    query(sqlQuery, [], function (error, records, fields) {
+        if (error) {
+            console.log(error);
+            resp.writeHead(500, {
+                'Content-Type': 'application/json'
+            });
+            resp.end(JSON.stringify({
+                success: false,
+                message: "Internal server error",
+            }));
+        } else {
+            if (records && records.length > 0) {
+                let data = {};
+                for (let i = 0; i < records.length; i++) {
+                    const record = records[i];
+                    if (data[record.propertyId] === undefined) {
+                        data[record.propertyId] = {};
+                        data[record.propertyId].amenities = new Set();
+                        data[record.propertyId].photoUrl = new Set();
+                        data[record.propertyId] = {
+                            propertyId: record.propertyId,
+                            headline: record.headline,
+                            propertyType: record.propertyType,
+                            bedroom: record.bedroom,
+                            bathroom: record.bedroom,
+                            accomodates: record.accomodates,
+                            street: record.street,
+                            city: record.city,
+                            state: record.state,
+                            zip: record.zip,
+                            country: record.country,
+                            oneNightRate: record.oneNightRate,
+                            username: record.username,
+                            propertyDescription: record.propertyDescription,
+                            bookingOption: record.bookingOption,
+                            minNightStay: record.minNightStay,
+                        };
+                    }
+
+                    if (record.amenity) {
+                        data[record.propertyId].amenities.add(amenity);
+                    }
+                    if (record.photoUrl) {
+                        data[record.propertyId].photoUrl.add(record.photoUrl);
+                    }
+                }
+                resp.writeHead(200, {
+                    'Content-Type': 'application/json'
+                });
+                resp.end(JSON.stringify({
+                    success: true,
+                    message: `result found`,
+                    results: data
+                }));
+            } else {
+                resp.writeHead(204, {
+                    'Content-Type': 'application/json'
+                });
+                resp.end(JSON.stringify({
+                    success: true,
+                    message: "No Content",
+                }));
+            }
+        }
+    });
+});
+function createDetailPropertyQuery(req) {
+    let { propertyId } = req.query;
+    return `select prop.*, photo.photoUrl from(
+        select p.*, a.amenity 
+        from property p  left join amenities a on p.propertyId = a.propertyId 
+        where p.markForDelete = 0 and isActive=1 and p.propertyId= ${propertyId}
+    ) prop left join (select * from propertyPhotos where markForDelete = 0) photo on prop.propertyId = photo.propertyId`;
+}
+function createListPropertiesQuery(req) {
+    let { filters, sortCriteria, sortOrder, arrivalDate, departureDate } = req.query;
+    let sqlQuery = `select prop.*, photo.photoUrl from(
+        select p.propertyId, p.headline, p.propertyType, p.bedroom, p.bathroom,
+        p.accomodates, p.street, p.city, p.state, p.zip, p.country, p.oneNightRate, 
+        a.amenity from property p left join amenities a
+        on p.propertyId = a.propertyId where  p.markForDelete = 0 and isActive=1`;
+    if (arrivalDate && departureDate) {
+        let diff = DateDiff(new Date(arrivalDate), new Date(departureDate));
+        sqlQuery += ` and (p.minNightStay = null or  p.minNightStay <= ${diff.days()}) and p.propertyId not in (
+            select distinct propertyId from propertyBlockDate 
+            where "${arrivalDate}"  between startDate and endDate or "${departureDate}"  between startDate and endDate  or
+            startDate between "${arrivalDate}" and "${departureDate}" or endDate between "${arrivalDate}" and "${departureDate}" )`;
+    }
+
+    let keys = Object.keys(filters);
+    if (filters && keys && keys.length > 0) {
+        for (let i = 0; i < keys.length; i++) {
+            let val = filters[keys[i]];
+            if (val instanceof Array && val.length > 0) {
+                if (val.every((str) => typeof str === 'string')) {
+                    sqlQuery += ` and ( ${keys[i]} in ("${val.join('", "')}") )`;
+                }
+                else {
+                    //in our usecase it will be number if not string
+                    sqlQuery += ` and ( ${keys[i]} in (${val.join()}) )`;
+                }
+            } else if (val.min) {
+                if (val.max) {
+                    if (typeof val.min === 'string' && typeof val.max === 'string') {
+                        sqlQuery += ` and ( ${keys[i]} between "${val.min}" and "${val.max}")`;
+                    } else {
+                        sqlQuery += ` and ( ${keys[i]} between ${val.min} and ${val.max})`;
+                    }
+                } else {
+                    if (typeof val.min === 'string') {
+                        sqlQuery += ` and ( ${keys[i]} >= "${val.min}")`;
+                    } else {
+                        sqlQuery += ` and ( ${keys[i]} >= ${val.min})`;
+                    }
+                }
+            } else if (val.max) {
+                if (typeof val.min === 'string') {
+                    sqlQuery += ` and ( ${keys[i]} <= "${val.max}")`;
+                } else {
+                    sqlQuery += ` and ( ${keys[i]} <= ${val.max})`;
+                }
+            } else if (typeof val === 'string') {
+                sqlQuery += ` and ( ${keys[i]} = "${val}" )`
+            } else {
+                sqlQuery += ` and ( ${keys[i]} = ${val} )`
+            }
+        }
+    }
+    sqlQuery += ` ) prop left join (select * from propertyPhotos where markForDelete = 0) photo on prop.propertyId = photo.propertyId `;
+    if (sortCriteria) {
+        sqlQuery += ` order by ${sortCriteria}`
+        if (sortOrder) {
+            sqlQuery += ` ${sortOrder}`
+        }
+    }
+    return sqlQuery;
+}
 function insertData(resp, data) {
     query("INSERT INTO property SET ?", data, function (error, records, fields) {
         if (error) {
