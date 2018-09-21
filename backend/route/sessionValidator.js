5abda27fb033088f09e09014b3404d475f3d7e96
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2018-09-20 17:28:37 -0700

    fixes in sessionValidator middleware

--- backend/route/sessionValidator.js
+++ backend/route/sessionValidator.js
@@ -1,19 +1,19 @@
 let express = require("express");
-let router = express.Router;
+let router = express.Router();
 
 router.use(function (req, response, next){
     if(req.session.username){
         next();
     }
     else{
-        resp.writeHead(401, {
+        response.writeHead(401, {
             'Content-Type': 'application/json'
         });
-        resp.end(JSON.stringify({
+        response.end(JSON.stringify({
             success: false,
             message: "User not logged in",
         }));
     }
 });
 
-module.exports.router = router;
+module.exports = router;
